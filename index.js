ab34d47c6b2a9f27cb977f8addedb05c44635da6
valbona1992 <valbona12@gmail.com>
2021-08-04 12:43:44 -0500

    add updates to JS

--- index.js
+++ index.js
@@ -1,6 +1,79 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const createMd = (answers, badge) => 
+    `
+# ${answers.title} 
+[license](https://img.shields.io/badge/license-${answers.badge})
+
+## Description
+${answers.description}
+
+## Table of Contents 
+  - [User Story](#userstory)
+  - [Demo Video](#demovideo)
+  - [Screenshots](screenshots)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributors](#contributors)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+## User Story
+${answers.userstory}
+
+## Demo Video 
+[link](${answers.demovideo})
+
+## Screenshots
+[Screenshot](${answers.screenshot})
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributors
+${answers.contributors}
+
+## License
+License is ${answers.license} standard license.
+
+## Tests
+${answers.test}
+
+## Questions
+ GitHub: https://github.com/${answers.github}  <br/>
+ Email me with any questions:: ${answers.email}
+ `;
+
+
+const badge = (license) => {
+    switch (license) {
+        case 'Apache license 2.0':
+            response = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+            break;
+        case 'MIT':
+            response = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+            break;
+        case 'IBM':
+            response = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+            break;
+        case 'Open Data':
+            response = '[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)'
+            break;
+        case 'None':
+            response = ' '
+            break;
+        default:
+            response = ' '
+            break;
+    }
+    return response;
+};
+
 inquirer
     .prompt([
         {
@@ -12,6 +85,28 @@ inquirer
             type: 'input',
             message: 'Enter a description of this project.',
             name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Enter the user story of this project.',
+            name: 'userstory',
+        },
+        {
+            type: 'input',
+            message: 'Enter the video demo link.',
+            name: 'demovideo',
+        },
+        {
+            type: 'input',
+            message: 'Enter screenshots for the project.',
+            name: 'firstscreenshot',
+            default: 'info'  // put link here 
+        },
+        {
+            type: 'input',
+            message: 'Enter another screenshot for the project.',
+            name: 'secondscreenshot',
+            default: 'info'  // put link here 
         },
         {
             type: 'input',
@@ -37,7 +132,7 @@ inquirer
             type: 'list',
             message: 'What license was used for this application?',
             name: 'license',
-            choices: ['None', 'GNU General v2', 'Apache license 2.0', 'MIT', 'IBM', 'Open Data'],
+            choices: ['None', 'Apache license 2.0', 'MIT', 'IBM', 'Open Data'],
         },
         {
             type: "input",
@@ -53,86 +148,7 @@ inquirer
     .then((answers) => {
         const generateMd = createMd(answers, badge);
 
-
-        fs.writeFile('README.md', generatedMd, (err) =>
+        fs.writeFile('README.md', generateMd, (err) =>
             err ? console.error(err) : console.log('Success!')
         );
     });
-
-
-
-const badge = (license) => {
-    switch (license) {
-        case 'Apache license 2.0':
-            response = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-            break;
-        case 'GNU General v2':
-            response = '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)'
-            break;
-        case 'MIT':
-            response = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-            break;
-        case 'IBM':
-            response = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
-            break;
-        case 'Open Data':
-            response = '[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)'
-            break;
-        case 'None':
-            response = ' '
-            break;
-        default:
-            response = ' '
-            break;
-    }
-    return response;
-},
-
-const createMd = ((answers, badge) => {
-    `
-        # <${answers.title}>
-${badge}
-
-## Description
-${answers.description}
-
-## Table of Contents 
-  - [User Story](#userstory)
-  - [Demo Video](#demovideo)
-  - [Screenshots](screenshots)
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [License](#license)
-  - [Contributors](#contributors)
-  - [Tests](#tests)
-  - [Questions](#questions)
-
-## User Story
-${answers.userstory}
-
-## Demo Video 
-${answers.demovideo}
-
-## Screenshots
-![Screenshot](${answers.screenshot})
-
-## Installation
-${answers.installation}
-
-## Usage
-${answers.usage}
-
-## Contributers
-${answers.contributoers}
-
-## License
-License is ${answers.license} standard license.
-
-## Tests
-${answers.test}
-
-## Questions
- GitHub: https://github.com/${answers.github}
- Email: ${answers.email}
-        `
-}
